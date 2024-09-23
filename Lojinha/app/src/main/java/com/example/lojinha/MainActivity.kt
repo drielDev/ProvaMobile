34bb2618cdfe0485585a13d1150f853270224253
dri <adrieltraningdev@gmail.com>
2024-09-23 09:24:54 -0300

    add navController

--- Lojinha/app/src/main/java/com/example/lojinha/MainActivity.kt
+++ Lojinha/app/src/main/java/com/example/lojinha/MainActivity.kt
@@ -5,10 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -19,7 +20,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.lojinha.ui.theme.LojinhaTheme
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,6 +39,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun LayoutMain() {
 
+    var navController = rememberNavController()
+
     var nome by remember { mutableStateOf("")}
     var categoria by remember { mutableStateOf("")}
     var preco by remember { mutableStateOf("")}
@@ -41,7 +48,7 @@ fun LayoutMain() {
     var listaProdutos by remember { mutableStateOf(listOf<Produto>()) }
 
     var precoFloat by remember { mutableStateOf(0.0f) }
-    var quantProdutoFloat by remember { mutableStateOf(0) }
+    var quantEstoqueFloat by remember { mutableStateOf(0) }
 
 
     Column(modifier = Modifier.fillMaxSize(),
@@ -50,7 +57,11 @@ fun LayoutMain() {
 
         TextField(value = nome, onValueChange = {nome = it}, label = { Text(text = "Nome")})
 
+        Spacer(modifier = Modifier.height(20.dp))
+
         TextField(value = categoria, onValueChange = {categoria = it}, label = { Text(text = "categoria")})
+
+        Spacer(modifier = Modifier.height(20.dp))
 
         TextField(
             value = preco,
@@ -61,14 +72,33 @@ fun LayoutMain() {
             label = { Text(text = "Preço") }
         )
 
+        Spacer(modifier = Modifier.height(20.dp))
+
         TextField(
             value = quantEstoque,
             onValueChange = {
                 quantEstoque = it
-                quantProdutoFloat = it.toIntOrNull() ?: 0 },
+                quantEstoqueFloat = it.toIntOrNull() ?: 0 },
             label = { Text(text = "Quantidade em Estoque") }
         )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(onClick = {
+            listaProdutos += Produto(nome, categoria, precoFloat, quantEstoqueFloat)
+            nome = ""
+            categoria = ""
+            precoFloat = 0.0f
+            quantEstoqueFloat = 0
+        }) {
+            Text(text = "Cadastrar")
+        }
     }
+}
+
+@Composable
+fun ProdutoActivity(){
+
 }
 
 @Preview(showBackground = true)

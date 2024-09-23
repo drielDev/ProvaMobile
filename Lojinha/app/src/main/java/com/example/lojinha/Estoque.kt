918c2a4e50f55163415e4154774dc8bcba6d7ee0
Adriel <adrielroquedev@gmail.com>
2024-09-23 15:34:53 -0300

    classe estoque

---
+++ Lojinha/app/src/main/java/com/example/lojinha/Estoque.kt
@@ -0,0 +1,30 @@
+package com.example.lojinha
+
+class Estoque {
+
+    companion object {
+        var listaEstoque = listOf<Produto>()
+
+        fun adicionarProdutos(nome: String, categoria: String, preco: Float, quant: Int) {
+            listaEstoque += listOf(Produto(nome, categoria, preco, quant))
+        }
+
+        fun calcularValorTotalEstoque(): Float {
+            var precoTotal = 0.0f
+            var quant = 0
+
+            listaEstoque.forEach { precoTotal += it.preco}
+            listaEstoque.forEach { quant += it.quantEstoque}
+
+            return precoTotal * quant
+        }
+
+        fun quantidadeTotalProdutos(): Int{
+            var quant = 0
+
+            listaEstoque.forEach { quant += it.quantEstoque}
+            return quant
+        }
+    }
+
+}
